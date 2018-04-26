db0554f791a03e6e7f20b73e96bf41b4cdc13eeb
bcollins <parietal.io@gmail.com>
2018-04-26 12:37:52 -0500

    added test method to __init__;

--- taj/__init__.py
+++ taj/__init__.py
@@ -4,5 +4,16 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-
 from .core import df_to_json, multidf_to_json  # NOQA
+
+
+def test():
+    """Run the pandas-taj test suite."""
+    import os
+    try:
+        import pytest
+    except ImportError:
+        import sys
+        sys.stderr.write("You need to install py.test to run tests.\n\n")
+        raise
+    pytest.main(os.path.dirname(__file__))
