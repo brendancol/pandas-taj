fc0666f5d2b321446164d3b79dfca230ca00dd4c
bcollins <parietal.io@gmail.com>
2018-05-14 10:07:52 -0500

    small change to test runnner

--- taj/__init__.py
+++ taj/__init__.py
@@ -1,19 +1,22 @@
 from __future__ import print_function, absolute_import
 
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from .core import df_to_json, multidf_to_json  # NOQA
 
+from .core import df_to_json, palettes
 
-def test():
-    """Run the pandas-taj test suite."""
-    import os
-    try:
-        import pytest
-    except ImportError:
-        import sys
-        sys.stderr.write("You need to install py.test to run tests.\n\n")
-        raise
-    pytest.main(os.path.dirname(__file__))
+from .test import test
+
+# def test():
+#     """Run the pandas-taj test suite."""
+#     import os
+#     try:
+#         import pytest
+#     except ImportError:
+#         import sys
+#         sys.stderr.write("You need to install py.test to run tests.\n\n")
+#         raise
+#     pytest.main(os.path.dirname(__file__))
